9e7939fa106cdb68008ff47294156f2ec1bfe09f
SkylarHammond55 <Skylarhammond55@gmail.com>
2023-07-07 13:52:38 -0500

    quiz questions

---
+++ script.js
@@ -0,0 +1,49 @@
+// Quiz questions and answers
+const quizQuestions = [
+  {
+    question: "Inside which HTML element do we put the JavaScript?",
+    choices: ["<script>", "<scripting>", "<js>", "<javascript>"],
+    correctAnswer: 0
+  },
+  {
+    question: "Which programming language is known as the 'language of the web'?",
+    choices: ["JavaScript", "Python", "Java", "C++"],
+    correctAnswer: 0
+  },
+  {
+    question: "Where is the correct place to insert a JavaScript?",
+    choices: ["The <head> section  ", "Both the <head> section and the <body> section are correct  ", "The <body> section"],
+    correctAnswer: 1
+  },
+  {
+    question: "The external JavaScript file must contain the <script> tag.",
+    choices: ["True", "False "],
+    correctAnswer: 1
+  },
+  {
+    question: "How do you write Hello World in an alert box?",
+    choices: ["alert(Hello World);", "msg(Hello World);", "msgBox(Hello World);", "alertBox(Hello World);"],
+    correctAnswer: 0
+  },
+  {
+    question: "How do you create a function in JavaScript?",
+    choices: ["function myFunction()  ", "function = myFunction()", "function:myFunction()",],
+    correctAnswer: 0
+  },
+  {
+    question: "How do you call a function named myFunction?",
+    choices: ["call myFunction()", "myFunction()", "call function myFunction()",],
+    correctAnswer: 1
+  },
+  {
+    question: "How to write an IF statement in JavaScript?",
+    choices: ["if i = 5 then  ", "if i = 5", "if i == 5 then", "if (i == 5) "],
+    correctAnswer: 3
+  },
+  {
+    question: "How can you add a comment in a JavaScript?",
+    choices: ["<!--This is a comment-->  ", "//This is a comment  ", "'This is a comment",],
+    correctAnswer: 1
+  },
+  // Add more quiz questions here
+];
